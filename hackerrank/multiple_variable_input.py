5c083386a409afbaafb5cd8b20d35cebbca08b46
Aldi Satria <aldisatria1996@gmail.com>
2021-09-23 11:33:52 +0700

    asd

---
+++ hackerrank/multiple_variable_input.py
@@ -0,0 +1,19 @@
+# a, b = [int(x) for x in input("Enter two values\n").split(', ')]
+# a, b = map(int, input("Enter two values\n").split(', '))
+a, b = input("Enter two values\n").split(', ')
+print("\nThe value of a is {} and b in {}".format(a, b))
+
+
+# L = [int(x) for x in input("Enter multiple values\n").split(', ')]
+# L = list(map(int, input("Enter multiple values\n").split(', ')))
+# print("\nThe values of input are", L)
+
+
+# nama, *hobi = map(str, input("Tulis nama dan macam2 hobi anda\n").split())
+# print(nama)
+# print(hobi)
+
+
+# name, *line = input().split()
+# print(name)
+# print(line)
--- hackerrank/nested_lists2.py
+++
@@ -1,5 +0,0 @@
-my_list = [["budi", 5], ["johan", 3], ["sarah", 4]]
-
-for x in my_list:
-    print(x[0])
-
